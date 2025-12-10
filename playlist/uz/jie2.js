cfc15611f6b5dde59693179ed61a656eaa4f5b13
kking966 <fre963254@outlook.com>
2025-12-10 19:06:15 +0800

    Update jie2.js

--- playlist/uz/jie2.js
+++ playlist/uz/jie2.js
@@ -1,10 +1,4 @@
 //@name:Jiejiesp
-//@version:3
-//@webSite:https://jiejiesp.xyz
-//@type:100
-//@isAV:1
-//@order:J
-//@remark:uz影视采集脚本（动态分类 + 调试日志）
 
 class Jiejiesp extends WebApiBase {
   constructor() {
